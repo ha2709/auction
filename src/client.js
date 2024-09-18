8fea72449e249da9ae408316921554fd938224c5
Henry Nguyen <ngpha@yahoo.com>
2024-09-18 15:03:14 +0700

    close auction

--- src/client.js
+++ src/client.js
@@ -1,116 +1,194 @@
-'use strict'
+"use strict";
 
-const RPC = require('@hyperswarm/rpc')
-const DHT = require('hyperdht')
-const Hypercore = require('hypercore')
-const Hyperbee = require('hyperbee')
-const crypto = require('crypto')
-const readline = require('readline')
+const RPC = require("@hyperswarm/rpc");
+const DHT = require("hyperdht");
+const Hypercore = require("hypercore");
+const Hyperbee = require("hyperbee");
+const crypto = require("crypto");
+const readline = require("readline");
 
 // Create readline interface for user interaction
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
 const peers = [
   // Add peers' public keys (hex) of other clients here for simplicity
-  { pubKey: Buffer.from('8ebcb4a46623edd4b539d8995275040a3581b8f0d3aafc52991d2c531c566326', 'hex') }
-]
+  {
+    pubKey: Buffer.from(
+      "8ebcb4a46623edd4b539d8995275040a3581b8f0d3aafc52991d2c531c566326",
+      "hex"
+    ),
+  },
+];
 
-const myClientId = crypto.randomBytes(16).toString('hex') // Generate a unique client ID
+const myClientId = crypto.randomBytes(16).toString("hex"); // Generate a unique client ID
 
 // Helper function to ask for input
 const askQuestion = (query) => {
-  return new Promise(resolve => rl.question(query, resolve))
-}
+  return new Promise((resolve) => rl.question(query, resolve));
+};
 
 const main = async () => {
   // hyperbee db
-   const dbPath = `./db/rpc-client-${myClientId}`
-  const hcore = new Hypercore(dbPath)
-  const hbee = new Hyperbee(hcore, { keyEncoding: 'utf-8', valueEncoding: 'binary' })
-  await hbee.ready()
+  const dbPath = `./db/rpc-client-${myClientId}`;
+  const hcore = new Hypercore(dbPath);
+  const hbee = new Hyperbee(hcore, {
+    keyEncoding: "utf-8",
+    valueEncoding: "binary",
+  });
+  await hbee.ready();
 
   // resolved distributed hash table seed for key pair
-  let dhtSeed = (await hbee.get('dht-seed'))?.value
+  let dhtSeed = (await hbee.get("dht-seed"))?.value;
   if (!dhtSeed) {
     // not found, generate and store in db
-    dhtSeed = crypto.randomBytes(32)
-    await hbee.put('dht-seed', dhtSeed)
+    dhtSeed = crypto.randomBytes(32);
+    await hbee.put("dht-seed", dhtSeed);
   }
 
   // start distributed hash table, it is used for rpc service discovery
   const dht = new DHT({
     port: 50001,
     keyPair: DHT.keyPair(dhtSeed),
-    bootstrap: [{ host: '127.0.0.1', port: 30001 }] // note boostrap points to dht that is started via cli
-  })
+    bootstrap: [{ host: "127.0.0.1", port: 30001 }], // note boostrap points to dht that is started via cli
+  });
 
   // public key of rpc server, used instead of address, the address is discovered via dht
-  const serverPubKey = Buffer.from('8ebcb4a46623edd4b539d8995275040a3581b8f0d3aafc52991d2c531c566326', 'hex')
+  // const serverPubKey = Buffer.from('8ebcb4a46623edd4b539d8995275040a3581b8f0d3aafc52991d2c531c566326', 'hex')
 
   // rpc lib
-  const rpc = new RPC({ dht })
+  const rpc = new RPC({ dht });
 
   // Setup RPC server
-  const rpcServer = rpc.createServer()
-  rpcServer.respond('auctionOpened', async (reqRaw) => {
-    const req = JSON.parse(reqRaw.toString('utf-8'))
-    console.log(`New auction opened with ID: ${req.auctionId}, Details: ${JSON.stringify(req.auctionDetails)}`)
-  })
+  const rpcServer = rpc.createServer();
+  rpcServer.respond("auctionOpened", async (reqRaw) => {
+    const req = JSON.parse(reqRaw.toString("utf-8"));
+    console.log(
+      `New auction opened with ID: ${req.auctionId}, Details: ${JSON.stringify(
+        req.auctionDetails
+      )}`
+    );
+  });
 
-  rpcServer.respond('newBid', async (reqRaw) => {
-    const req = JSON.parse(reqRaw.toString('utf-8'))
-    console.log(`New bid submitted for auction ${req.auctionId}: ${req.bid.amount} by ${req.bid.bidder}`)
-  })
+  rpcServer.respond("newBid", async (reqRaw) => {
+    const req = JSON.parse(reqRaw.toString("utf-8"));
+    console.log(
+      `New bid submitted for auction ${req.auctionId}: ${req.bid.price} by ${req.bid.bidder}`
+    );
+  });
 
-  rpcServer.respond('auctionClosed', async (reqRaw) => {
-    const req = JSON.parse(reqRaw.toString('utf-8'))
-    console.log(`Auction ${req.auctionId} closed with winner: ${JSON.stringify(req.winner)}`)
-  })
-  await rpcServer.listen()
+  rpcServer.respond("auctionClosed", async (reqRaw) => {
+    const req = JSON.parse(reqRaw.toString("utf-8"));
+    console.log(
+      `Auction ${req.auctionId} closed with winner: ${JSON.stringify(
+        req.winner
+      )}`
+    );
+  });
 
-  console.log(`RPC server listening at: ${rpcServer.publicKey.toString('hex')}`)
+  await rpcServer.listen();
+
+  console.log(
+    `RPC server listening at: ${rpcServer.publicKey.toString("hex")}`
+  );
 
   // payload for request
-  const payload = { nonce: 126 }
-  const payloadRaw = Buffer.from(JSON.stringify(payload), 'utf-8')
-  const notifyAuctionOpened = async (auctionId, auctionDetails) => {
-    const payload = { auctionId, auctionDetails };
-    const payloadRaw = Buffer.from(JSON.stringify(payload), 'utf-8');
-  
-    // Assuming you have a list of known peers
+  // const payload = { nonce: 126 }
+
+  const notifyAuctionOpened = async (
+    auctionId,
+    auctionDetails,
+    initialPrice
+  ) => {
+    const payload = {
+      auctionId,
+      auctionDetails,
+      initialPrice,
+      creator: myClientId,
+    };
+    const payloadRaw = Buffer.from(JSON.stringify(payload), "utf-8");
+
+    // Send the auction creation request to peers
     for (const peer of peers) {
-      await rpc.request(peer.pubKey, 'auctionOpened', payloadRaw);
+      try {
+        const responseRaw = await rpc.request(
+          peer.pubKey,
+          "auctionOpened",
+          payloadRaw
+        );
+        const response = JSON.parse(responseRaw.toString("utf-8"));
+        console.log(`Response from peer: ${JSON.stringify(response)}`);
+      } catch (error) {
+        console.error(
+          `Error creating auction with peer ${peer.pubKey.toString("hex")}:`,
+          error
+        );
+      }
     }
-  
-    console.log(`Auction ${auctionId} opened and notified other parties.`);
+
+    // console.log(`Auction ${auctionId} opened and notified other parties.`);
   };
-  
-  // Submit a bid and notify peers
-  const submitBid = async (auctionId, bidAmount) => {
-    const payload = { auctionId, bid: { amount: bidAmount, bidder: myClientId } }
-    const payloadRaw = Buffer.from(JSON.stringify(payload), 'utf-8')
 
+  // Submit a bid and notify peers
+  const submitBid = async (auctionId, bidPrice) => {
+    const payload = { auctionId, bid: { price: bidPrice, bidder: myClientId } };
+    const payloadRaw = Buffer.from(JSON.stringify(payload), "utf-8");
+
+    // Send the auction creation request to peers
     for (const peer of peers) {
-      await rpc.request(peer.pubKey, 'newBid', payloadRaw)
+      try {
+        const responseRaw = await rpc.request(
+          peer.pubKey,
+          "newBid",
+          payloadRaw
+        );
+        const response = JSON.parse(responseRaw.toString("utf-8"));
+        console.log(`Response from peer: ${JSON.stringify(response)}`);
+      } catch (error) {
+        console.error(
+          `Error creating auction with peer ${peer.pubKey.toString("hex")}:`,
+          error
+        );
+      }
     }
 
-    console.log(`Bid submitted for auction ${auctionId}: ${bidAmount}`)
-  }
+    // console.log(`Bid submitted for auction ${auctionId}: ${bidPrice}`)
+  };
 
   // Close an auction and propagate the result
-  const closeAuction = async (auctionId, winner) => {
-    const payload = { auctionId, winner }
-    const payloadRaw = Buffer.from(JSON.stringify(payload), 'utf-8')
+  const closeAuction = async (auctionId) => {
+    const payload = { auctionId, callerId: myClientId };
+    const payloadRaw = Buffer.from(JSON.stringify(payload), "utf-8");
 
     for (const peer of peers) {
-      await rpc.request(peer.pubKey, 'auctionClosed', payloadRaw)
+      try {
+        // Send request to close the auction to all peers (including the one responsible for closing)
+        const responseRaw = await rpc.request(
+          peer.pubKey,
+          "auctionClosed",
+          payloadRaw
+        );
+        const response = JSON.parse(responseRaw.toString("utf-8"));
+
+        console.log(
+          `Response from peer ${peer.pubKey.toString("hex")}: ${JSON.stringify(
+            response
+          )}`
+        );
+      } catch (error) {
+        console.error(
+          `Error sending auction close request to peer ${peer.pubKey.toString(
+            "hex"
+          )}:`,
+          error
+        );
+      }
     }
 
-    console.log(`Auction ${auctionId} closed and result propagated.`)
-  }
+    console.log(`Auction ${auctionId} closed and result propagated.`);
+  };
 
   // sending request and handling response
   // see console output on server code for public key as this changes on different instances
@@ -123,29 +201,36 @@ const main = async () => {
   // await dht.destroy()
 
   while (true) {
-    const action = await askQuestion('Choose an action: (1) Open Auction, (2) Place Bid, (3) Close Auction: ')
+    const action = await askQuestion(
+      "Choose an action: (1) Open Auction, (2) Place Bid, (3) Close Auction: "
+    );
 
-    if (action === '1') {
+    if (action === "1") {
       // Open Auction
-      const auctionDetails = await askQuestion('Enter auction details (e.g., "Selling a picture for 50 USDt"): ')
-      const auctionId = crypto.randomBytes(16).toString('hex') // Unique auction ID
-      await notifyAuctionOpened(auctionId, auctionDetails)
-
-    } else if (action === '2') {
+      const auctionDetails = await askQuestion(
+        'Enter auction details (e.g., "Selling a picture for 50 USDt"): '
+      );
+      const auctionId = crypto.randomBytes(16).toString("hex"); // Unique auction ID
+      const initialPrice = await askQuestion("Enter the initial price: ");
+      await notifyAuctionOpened(
+        auctionId,
+        auctionDetails,
+        parseFloat(initialPrice)
+      );
+    } else if (action === "2") {
       // Place Bid
-      const auctionId = await askQuestion('Enter the auction ID to bid on: ')
-      const bidAmount = await askQuestion('Enter your bid amount: ')
-      await submitBid(auctionId, bidAmount)
-
-    } else if (action === '3') {
+      const auctionId = await askQuestion("Enter the auction ID to bid on: ");
+      const bidPrice = await askQuestion("Enter your bid price: ");
+      await submitBid(auctionId, parseFloat(bidPrice));
+    } else if (action === "3") {
       // Close Auction
-      const auctionId = await askQuestion('Enter the auction ID to close: ')
-      const winner = { amount: 100, bidder: myClientId }  
-      await closeAuction(auctionId, winner)
+      const auctionId = await askQuestion("Enter the auction ID to close: ");
+
+      await closeAuction(auctionId);
     } else {
-      console.log('Invalid action. Please choose 1, 2, or 3.')
+      console.log("Invalid action. Please choose 1, 2, or 3.");
     }
   }
-}
+};
 
-main().catch(console.error)
+main().catch(console.error);
--- utils/index.js
+++ utils/index.js
@@ -1,67 +1,175 @@
 class Auction {
-    constructor(hbee) {
-      this.hbee = hbee;  // Hyperbee instance to store auction data
-    }
-  
-    async createAuction(auctionId, auctionDetails) {
-      const auction = {
+  constructor(hbee) {
+    this.hbee = hbee; // Hyperbee instance to store auction data
+  }
+
+  async createAuction(auctionId, auctionDetails, creatorId, initialPrice) {
+    const auction = {
+      id: auctionId,
+      details: auctionDetails,
+      creator: creatorId, // Store the creator of the auction
+      initialPrice: initialPrice,
+      bids: [],
+      winner: null,
+    };
+    // Store auction in Hyperbee
+    await this.hbee.put(auctionId, JSON.stringify(auction));
+    console.log(`Auction created with ID: ${auctionId}`);
+    // Return the auction data as a JSON object
+    return {
+      status: "success",
+      message: "Auction created successfully",
+      auction: {
         id: auctionId,
         details: auctionDetails,
+        creator: creatorId,
+        initialPrice: initialPrice,
         bids: [],
-        winner: null
-      };
-      // Store auction in Hyperbee
-      await this.hbee.put(auctionId, JSON.stringify(auction));
-      console.log(`Auction created with ID: ${auctionId}`);
-    }
-  
-    async placeBid(auctionId, bid) {
+        winner: null,
+      },
+    };
+  }
+
+  async placeBid(auctionId, bid) {
+    try {
       // Retrieve the auction
       const auctionEntry = await this.hbee.get(auctionId);
       if (!auctionEntry) {
-        throw new Error('Auction not found');
+        return {
+          status: "error",
+          message: `Auction with ID ${auctionId} not found`,
+        };
       }
-  
+
       // Parse the auction data
       const auction = JSON.parse(auctionEntry.value);
-      
+
+      // Check if the new bid is higher than the initial price or the current highest bid
+      let highestBidPrice = auction.initialPrice;
+
+      if (auction.bids && auction.bids.length > 0) {
+        // Find the current highest bid
+        const highestBid = auction.bids.reduce((max, currentBid) => {
+          return currentBid.price > max.price ? currentBid : max;
+        }, auction.bids[0]);
+
+        highestBidPrice = highestBid.price;
+      }
+
+      // Compare the new bid with the highest bid
+      if (parseFloat(bid.price) <= parseFloat(highestBidPrice)) {
+        console.log(
+          `Bid rejected: ${JSON.stringify(
+            bid
+          )}. The new bid must be higher than the current highest bid or the initial price of ${highestBidPrice} USDt.`
+        );
+        return {
+          status: "faill",
+          message: `The new bid must be higher than the current highest bid or the initial price of ${highestBidPrice} USDt`,
+          auction: {
+            id: auctionId,
+            details: auction.details,
+            creator: auction.creator,
+            initialPrice: auction.initialPrice,
+            bids: auction.bids, // Updated list of bids
+            winner: auction.winner,
+          },
+        };
+      }
       // Add the bid to the auction
       auction.bids.push(bid);
-      
+
       // Update the auction in the database
       await this.hbee.put(auctionId, JSON.stringify(auction));
       console.log(`Bid placed: ${JSON.stringify(bid)}`);
+      return {
+        status: "success",
+        message: "Bid placed successfully",
+        auction: {
+          id: auctionId,
+          details: auction.details,
+          creator: auction.creator,
+          initialPrice: auction.initialPrice,
+          bids: auction.bids, // Updated list of bids
+          winner: auction.winner,
+        },
+      };
+    } catch (error) {
+      console.error("Error handling auctionOpened:", error);
+
+      // Return an error response
+      return Buffer.from(
+        JSON.stringify({
+          status: "error",
+          message: error.message,
+        }),
+        "utf-8"
+      );
     }
-  
-    async closeAuction(auctionId) {
+  }
+
+  // Close the auction, only if the caller is the creator
+  async closeAuction(auctionId, callerId) {
+    try {
       // Retrieve the auction
       const auctionEntry = await this.hbee.get(auctionId);
       if (!auctionEntry) {
-        throw new Error('Auction not found');
+        console.log("Auction not found");
+        return {
+          status: "error",
+          message: `Auction with ID ${auctionId} not found`,
+        };
       }
-  
+
       // Parse the auction data
-      const auctionData = JSON.parse(auctionEntry.value);
-      
- 
-    // Find the highest bid
-    const highestBid = auctionData.bids.reduce((max, bid) => {
-        return bid.amount > max.amount ? bid : max;
-    }, auctionData.bids[0]);  // Start with the first bid as the highest bid
+      const auction = JSON.parse(auctionEntry.value);
+      console.log(126, auction);
+      // Check if the caller is the creator of the auction
+      //   if (auction.creator !== callerId) {
+      //     console.log("Only the creator of the auction can close it.");
+      //     return;
+      //   }
 
-    console.log(`Highest bid found: ${JSON.stringify(highestBid)}`);
+      // Check if there are any bids to select a winner
+      if (!auction.bids || auction.bids.length === 0) {
+        console.log("Auction cannot be closed as there are no bids.");
+        return;
+      }
 
-    // Mark the highest bid as the winner
-    const winner = highestBid;
+      // Find the highest bid (the winner)
+      const winner = auction.bids.reduce((max, currentBid) => {
+        return currentBid.price > max.price ? currentBid : max;
+      }, auction.bids[0]);
+      console.log(143, winner);
+      // Set the winner in the auction
+      auction.winner = winner;
 
-    // Update the auction with the winner
-    auctionData.winner = winner;
-    await hbee.put(auctionId, JSON.stringify(auctionData));
-
-    console.log(`Auction ${auctionId} closed with winner: ${JSON.stringify(winner)}`);
-
- 
+      // Update the auction in the database
+      await this.hbee.put(auctionId, JSON.stringify(auction));
+      console.log(
+        `Auction ${auctionId} closed. Winner: ${JSON.stringify(winner)}`
+      );
+      // Return the auction data as a JSON response
+      return {
+        status: "success",
+        message: "Auction closed successfully",
+        auction: {
+          id: auctionId,
+          details: auction.details,
+          creator: auction.creator,
+          initialPrice: auction.initialPrice,
+          bids: auction.bids,
+          winner: auction.winner,
+        },
+      };
+    } catch (error) {
+      console.error("Error closing auction:", error);
+      return {
+        status: "error",
+        message: error.message,
+      };
     }
   }
+}
 // Export the Auction class
-module.exports = Auction;  
+module.exports = Auction;
